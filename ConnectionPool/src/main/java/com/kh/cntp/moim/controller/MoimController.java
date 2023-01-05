a4a6fbd8d32e0efb97c840a8d471a94d0dc8f7be
zoni613 <121650369+zoni613@users.noreply.github.com>
2023-01-05 14:03:55 +0900

    [세정] MoimController 빈등록 및 teamEnrollFrom 메소드 작성
    매핑값은 나중에 정해지면 수정하겠습니다.

--- ConnectionPool/src/main/java/com/kh/cntp/moim/controller/MoimController.java
+++ ConnectionPool/src/main/java/com/kh/cntp/moim/controller/MoimController.java
@@ -1,13 +1,15 @@
 package com.kh.cntp.moim.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 public class MoimController {
 	
 	// 임시적으로 moim 줄여서 mm해뒀는데 나중에 바꿔요 ....
 	@RequestMapping("teamEnrollForm.mm")
 	public String teamEnrollFrom() {
-		return "moim/teamEnrollFrom";
+		return "moim/teamEnrollForm";
 	}
 
 }
