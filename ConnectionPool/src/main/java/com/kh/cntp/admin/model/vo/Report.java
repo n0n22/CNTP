d6d35ee1ce5a9c471faa905d1dd1486731a942af
n0n22 <121650474+n0n22@users.noreply.github.com>
2023-01-07 17:03:08 +0900

    VO 생성 - 성경

---
+++ ConnectionPool/src/main/java/com/kh/cntp/admin/model/vo/Report.java
@@ -0,0 +1,22 @@
+package com.kh.cntp.admin.model.vo;
+
+import lombok.Data;
+
+@Data
+public class Report {
+	
+	private int reportNo;			// 신고번호
+	private int memNo;				// 신고대상자 회원번호
+	private String reportBoard;		// 신고대상글유형
+	private String reportDate;		// 신고일
+	private int boardNo;			// 신고대상글번호
+	private String reportReason;	// 신고사유
+	private int reporter;			// 신고자 회원번호
+	private String completionDate;	// 처리일
+	private String reportResult;	// 처리결과
+	private String memNickname;		// 신고대상자 닉네임
+	private String reporterName;	// 신고자 닉네임
+	
+	
+
+}
