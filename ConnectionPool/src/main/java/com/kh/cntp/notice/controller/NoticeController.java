f18095b85faea2d47dfac382196d567dd560a3a6
n0n22 <121650474+n0n22@users.noreply.github.com>
2023-01-09 10:40:31 +0900

    menubar.jsp 수정 - 성경
    taglib / alert창 넣음

--- ConnectionPool/src/main/java/com/kh/cntp/notice/controller/NoticeController.java
+++ ConnectionPool/src/main/java/com/kh/cntp/notice/controller/NoticeController.java
@@ -1,5 +1,7 @@
 package com.kh.cntp.notice.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,8 +11,9 @@ public class NoticeController {
 	
 	// 공지사항 목록 조회 -> 공지사항 목록 페이지로 이동
 	@RequestMapping("list.no")
-	public String selectNoticeList() {
+	public String selectNoticeList(HttpSession session) {
 		
+		session.setAttribute("alertMsg", "알럿테스트");
 		
 		return "notice/noticeList";
 	}
