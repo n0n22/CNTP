57bd9bbb01be82177a8cc385ac3749362ae13528
kyungho-SHIN <121650450+kyungho-SHIN@users.noreply.github.com>
2023-01-09 10:06:40 +0900

    member VO Date -> String으로 변경 - 경호

--- ConnectionPool/src/main/java/com/kh/cntp/member/model/vo/Member.java
+++ ConnectionPool/src/main/java/com/kh/cntp/member/model/vo/Member.java
@@ -18,12 +18,13 @@ public class Member {
 	private String memName; //	MEM_NAME	VARCHAR2(10 BYTE)
 	private String memArea; //	MEM_AREA	VARCHAR2(10 BYTE)
 	private String phone; //	PHONE	VARCHAR2(13 BYTE)
-	private Date birthDay; //	BIRTHDAY	DATE
+	private String birthDay; //	BIRTHDAY	DATE
 	private String detailArea; //	DETAIL_AREA	VARCHAR2(30 BYTE)
 	private String memStatus; //	MEM_STATUS	CHAR(1 BYTE)
-	private Date enrollDate; //	ENROLL_DATE	DATE
+	private String enrollDate; //	ENROLL_DATE	DATE
 	private String grade; //	GRADE	CHAR(1 BYTE)
-	private Date endDate; //  END_DATE DATE 일단 데이트로 받아보고 어려우면 스트링으로 변경하겠습니다	
+	private String endDate; //  END_DATE DATE 일단 데이트로 받아보고 어려우면 스트링으로 변경하겠습니다	
+	
 	
 	
 
