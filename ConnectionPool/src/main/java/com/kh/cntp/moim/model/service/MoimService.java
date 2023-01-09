a8b330019f28b84b5ed1fe536711b7f1dedc89a0
zoni613 <121650369+zoni613@users.noreply.github.com>
2023-01-09 11:38:41 +0900

    [세정] MoimService 수정

--- ConnectionPool/src/main/java/com/kh/cntp/moim/model/service/MoimService.java
+++ ConnectionPool/src/main/java/com/kh/cntp/moim/model/service/MoimService.java
@@ -1,5 +1,48 @@
 package com.kh.cntp.moim.model.service;
 
-public interface MoimService {
+import com.kh.cntp.moim.model.vo.Apply;
+import com.kh.cntp.moim.model.vo.Team;
+import com.kh.cntp.moim.model.vo.TeamMember;
 
+public interface MoimService {
+	
+	
+	// --------------- 팀 등록 관련 메소드 (teamEnrollForm) ---------------
+	
+	// teamEnrollForm 페이지에서 비동기식으로 팀 이름 중복 체크
+	String ajaxSelectTeam(String checkName);
+	
+	// 팀 등록
+	int insertTeam(Team team);
+	
+	// 팀 등록 성공 시 팀장 insert, 후에 신청 수락 시에도 team_grade에 M 담아서 해당 메소드 사용
+	int insertTeamMember(TeamMember teamMember);
+	
+	// 팀 등록 성공 시  + point 메소드도 하는 거 잊지 말기!
+	int insertResultHistory(int teamNo);
+	
+	
+	//  --------------- 팀 상세 페이지 조회 관련 메소드 (teamPage) ---------------
+	// 팀 정보 select
+	Team selectTeam(int teamNo);
+	
+	// teamMember select
+	TeamMember selectTeamMember(int teamNo);
+	
+	// --------------- 
+	// apply 관련 정보 select
+	// 후에 group 상세 페이지에서도 중복 사용
+	Apply selectApply(int moimNo);
+	
+	// 신청하는 메소드
+	// 신청하는 loginUser 번호, 팀 번호로다가
+	int insertApply(Team team);
+	
+	
+	
+	
+	
+	
+	
+	
 }
--- ConnectionPool/src/main/java/com/kh/cntp/moim/model/service/MoimServiceImpl.java
+++ ConnectionPool/src/main/java/com/kh/cntp/moim/model/service/MoimServiceImpl.java
@@ -1,5 +1,19 @@
 package com.kh.cntp.moim.model.service;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kh.cntp.moim.model.dao.MoimDao;
+
+@Service
 public class MoimServiceImpl implements MoimService {
+	
+	@Autowired
+	private MoimDao moimDao;
+	
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+	
 
 }
