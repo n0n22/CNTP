c31d3d699a8a0d255564fe9e0993f8bb2a83befa
ImSeon01 <121650379+ImSeon01@users.noreply.github.com>
2023-01-09 10:39:00 +0900

    [임선혁] 배틀 MVC 구조 완성 Battle, BattleResult, PoolInfo, ResultHistory

---
+++ ConnectionPool/src/main/java/com/kh/cntp/battle/model/dao/BattleDao.java
@@ -0,0 +1,9 @@
+package com.kh.cntp.battle.model.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BattleDao {
+	
+
+}
---
+++ ConnectionPool/src/main/java/com/kh/cntp/battle/model/service/BattleService.java
@@ -0,0 +1,16 @@
+package com.kh.cntp.battle.model.service;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kh.cntp.battle.model.dao.BattleDao;
+
+public interface BattleService {
+
+	
+	
+	
+	
+
+}
