2aa7f43b959748927e0c069c4fe17b6b957c923e
n0n22 <121650474+n0n22@users.noreply.github.com>
2023-01-09 12:07:28 +0900

    공지사항 select 추가 중 - 성경

--- ConnectionPool/src/main/java/com/kh/cntp/notice/model/dao/NoticeDao.java
+++ ConnectionPool/src/main/java/com/kh/cntp/notice/model/dao/NoticeDao.java
@@ -1,10 +1,15 @@
 package com.kh.cntp.notice.model.dao;
 
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class NoticeDao {
 
+	public int selectListCount(SqlSessionTemplate sqlSession) {
+		return sqlSession.selectOne("adminMapper.selectListCount");
+	}
+	
 	
 	
 	
--- ConnectionPool/src/main/java/com/kh/cntp/notice/model/service/NoticeServiceImpl.java
+++ ConnectionPool/src/main/java/com/kh/cntp/notice/model/service/NoticeServiceImpl.java
@@ -1,9 +1,25 @@
 package com.kh.cntp.notice.model.service;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.kh.cntp.notice.model.dao.NoticeDao;
+
 @Service
-public class NoticeServiceImpl {
+public class NoticeServiceImpl implements NoticeService {
+
+	@Autowired
+	private NoticeDao noticeDao;
+	
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+	
+	
+	@Override
+	public int selectListCount() {
+		return noticeDao.selectListCount(sqlSession);
+	}
 
 	
 	
