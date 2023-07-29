630ec09edfa59b7fbfec73b8851a6bd12d4da125
kyungho-SHIN <121650450+kyungho-SHIN@users.noreply.github.com>
2023-07-30 03:13:35 +0900

    Cert Scheduled 수정 및 테스트 - 경호
    Cert Scheduled 작동 시간 주기 및 쿼리문 오류 수정

--- ConnectionPool/src/main/java/com/kh/cntp/member/scheduler/CertScheduler.java
+++ ConnectionPool/src/main/java/com/kh/cntp/member/scheduler/CertScheduler.java
@@ -2,19 +2,25 @@ package com.kh.cntp.member.scheduler;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
 import com.kh.cntp.member.model.service.MemberService;
 
+@Component
 public class CertScheduler {
 
     //Scheduling 기능을 적용하기 위해 작성한 service나 dao 등을 불러와서 작성
-    
     @Autowired
     private MemberService memberService;
 	
-    //1시간마다 DELETE
-    @Scheduled(cron = "0 0 0/1 1/1 * ?")
+    //매주 월요일 12시 실행
+    @Scheduled(cron = "* 0 12 * * MON")
     public void certDeleteAll() {
-    	memberService.certDeleteAll();
+    	if(memberService.certDeleteAll() > 0) {
+    		System.out.println("CertTable Delect success");
+    	} else {
+    		System.out.println("CertTable Delect fail");
+    	}
+    	
     }
 }
